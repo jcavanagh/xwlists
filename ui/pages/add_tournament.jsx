ee18b883a3707846d73b77a74fa3f53dee9f7a24
Joe Cavanagh <joe@jlcavanagh.com>
2017-02-18 23:55:40 -0800

    Redux, decorators, starting forms

--- ui/pages/add_tournament.jsx
+++ ui/pages/add_tournament.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { Field, reduxForm as ReduxForm } from 'redux-form';
 
-export default class AddTournament extends React.Component {
+import DatePicker from 'material-ui/DatePicker';
+
+class FormDatePicker extends React.Component {
+    onDateChange = (evt, date) => {
+        //Format how backend wants it
+        const formatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        this.props.input.onChange(formatted);
+    }
+
     render() {
-        return <h1>AddTournament</h1>;
+        const { input: { value } } = this.props;
+        const asDate = value ? new Date(value) : new Date();
+        return (
+            <DatePicker hintText="Select a Date" container="inline" mode="landscape" autoOk={true} onChange={this.onDateChange} value={asDate} />
+        )
     }
 }
+
+@ReduxForm({
+    form: 'addTourney'
+})
+class AddTournament extends React.Component {
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit}>
+                <div>
+                    <label htmlFor="name">Tourney Name</label>
+                    <Field name="name" component="input" type="text"/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email (Private)</label>
+                    <Field name="email" component="input" type="email"/>
+                </div>
+                <div>
+                    <label htmlFor="tourney_format_dropdown">Tourney Format</label>
+                    <Field name="tourney_format_dropdown" component="select" />
+                    <label htmlFor="tourney_format_custom">Or, enter a custom format</label>
+                    <Field name="tourney_format_custom" component="input" type="text" />
+                </div>
+                <div>
+                    <label htmlFor="datepicker">Date Played</label>
+                    <Field name="datepicker" component={FormDatePicker} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default AddTournament;
--- ui/xwlists.js
+++ ui/xwlists.js
@@ -2,24 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Router, Route, Link, browserHistory } from 'react-router';
 
-import App from './app';
-import { About, AddTournament, Analysis, Search, NotFound, Tournaments, Vassal, WorldMap } from 'pages/index';
-
+//Material UI needs this
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
+//Actual app
+import App from './app';
+import { About, AddTournament, Analysis, Search, NotFound, Tournaments, Vassal, WorldMap } from 'pages/index';
+
 ReactDOM.render((
-    <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Search} />
-            <Route path="about" component={About} />
-            <Route path="new" component={AddTournament} />
-            <Route path="search" component={Search} />
-            <Route path="heatmap" component={WorldMap} />
-            <Route path="league" component={Vassal} />
-            <Route path="time_series" component={Analysis} />
-            <Route path="tourneys" component={Tournaments} />
-            <Route path="*" component={NotFound} />
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Search} />
+                <Route path="about" component={About} />
+                <Route path="new" component={AddTournament} />
+                <Route path="search" component={Search} />
+                <Route path="heatmap" component={WorldMap} />
+                <Route path="league" component={Vassal} />
+                <Route path="time_series" component={Analysis} />
+                <Route path="tourneys" component={Tournaments} />
+                <Route path="*" component={NotFound} />
+            </Route>
+        </Router>
+    </Provider>
 ), document.getElementById('xwlists'));
